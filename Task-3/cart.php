3c9bf71dd9850be5a63157804e07ea8c5b680876
Zain Jamshaid <zainjamshaid25@gmail.com>
2026-03-11 05:51:40 +0500

    Update cart.php

--- Task-3/cart.php
+++ Task-3/cart.php
@@ -1,6 +1,14 @@
 <?php
 session_start();
 
+if (isset($_GET["clear"])) {
+    session_start();
+    session_unset();
+    session_destroy();
+    header("Location: cart.php");
+    exit;
+}
+
 $products = [
     "1" => ["name" => "Shoes", "price" => "1000"],
     "2" => ["name" => "Jacket", "price" => "2000"],
@@ -23,3 +31,4 @@ if (!isset($_SESSION["cart"]) || empty($_SESSION["cart"])) {
 }
 ?>
 <br/><a href="shop.php">Continue Shopping</a>
+<br/><a href="cart.php?clear=1">Clear Cart</a>
